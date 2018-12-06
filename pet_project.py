21bae8cdb36ca9d175fed27157a2ed68cbb47c48
Peter Jano <peter.jano88@gmail.com>
2018-12-06 21:06:40 +0100

    modified printing and looks with the price

--- pet_project.py
+++ pet_project.py
@@ -12,19 +12,26 @@ soup = BeautifulSoup(source, "html5lib")
 # csv_file = open('steam_scrape.csv', 'w')
 
 
+print('\n')
+
 for discounts in soup.find_all('a', class_ = 'search_result_row'):
     time.sleep(2)
     
     name = discounts.find('span', class_='title').text
-    print(name)
+    print(name + '\n')
 
     on_sale = discounts.find('div', class_='search_discount').span.text
     print(on_sale)
 
     price = discounts.find('div', class_= 'search_price').text
-    for letter in price:
-        if letter
-    print(price.split('€'))
+    print('Original price\tOn sale price')
+
+    price = price.replace('\n\t\t\t\t\t\t\t\t', '')
+    price = price.replace('€', '€\t\t')
+    # for letter in price:
+    #     if letter
+    print(price)
+    print('\n')
 
 # print(soup.prettify())
 
