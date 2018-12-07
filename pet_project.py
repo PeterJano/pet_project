3adb899921dc7e1e235484a3362fffb4a41c7282
Peter Jano <peter.jano88@gmail.com>
2018-12-07 13:13:32 +0100

    Header added, CSV export started, not working properly

--- pet_project.py
+++ pet_project.py
@@ -7,17 +7,27 @@ import sys
 import os
 import random
 
+os.system("clear")
+
+def print_header():
+    print('''
+\t\t\t\t███████╗████████╗███████╗ █████╗ ███╗   ███╗    ███████╗ █████╗ ██╗     ███████╗
+\t\t\t\t██╔════╝╚══██╔══╝██╔════╝██╔══██╗████╗ ████║    ██╔════╝██╔══██╗██║     ██╔════╝
+\t\t\t\t███████╗   ██║   █████╗  ███████║██╔████╔██║    ███████╗███████║██║     █████╗  
+\t\t\t\t╚════██║   ██║   ██╔══╝  ██╔══██║██║╚██╔╝██║    ╚════██║██╔══██║██║     ██╔══╝  
+\t\t\t\t███████║   ██║   ███████╗██║  ██║██║ ╚═╝ ██║    ███████║██║  ██║███████╗███████╗
+\t\t\t\t╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝    ╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝ ''')
 
 def menu(soup):
 
     choice = " "
-    while choice not in '1 2 3 4 666'.split():
-        os.system("clear")
-        choice = input("{:>300}".format(" Choose an option!\n\n") +
-                            "{:>295}".format("(1) Show all\n") +
-                            "{:>302}".format("(2) Filter by price\n") +
-                            "{:>301}".format("(3) Filter by sale\n") +
-                            "{:>292}".format("(q) Quit\n\n"))
+    while choice not in '1 2 3 4 5 666'.split():
+        choice = input("{:>250}".format(" Choose an option!\n\n") +
+                            "{:>246}".format("(1) Show all\n") +
+                            "{:>252}".format("(2) Filter by sale\n") +
+                            "{:>253}".format("(3) Filter by price\n") +
+                            "{:>251}".format("(4) Export to CSV\n") +
+                            "{:>243}".format("(q) Quit\n\n"))
 
         if choice == "1":
             show_all(soup)  # Ide gyün a show all fgv.
@@ -26,8 +36,10 @@ def menu(soup):
             lowest_discount *= -1
             show_filtered_by_sale(soup, lowest_discount)  # Filter by price fgv.
         elif choice == "3":
-            lowest_price = input("Please enter the minimum desired price: ") 
-            show_filtered_by_price(soup, lowest_price)  # filter by sale
+            highest_price = input("Please enter the minimum desired price: ") 
+            show_filtered_by_price(soup, highest_price)  # filter by sale
+        elif choice == "4":
+            export_to_csv(soup)
         elif choice == "q":
             sys.exit()
         elif choice == "666":
@@ -46,12 +58,10 @@ def making_soup():
 
 def show_all(soup):
     for discounts in soup.find_all('a', class_ = 'search_result_row'):
-        time.sleep(random.randint(2,4))        
-
+        time.sleep(random.randint(2,4))
         name = discounts.find('span', class_='title').text
         on_sale = discounts.find('div', class_='search_discount').span.text
         price = discounts.find('div', class_= 'search_price').text
-
 
         print(name + '\n')
         print(on_sale)
@@ -65,12 +75,12 @@ def show_all(soup):
 def show_filtered_by_price(soup, number):
     for discounts in soup.find_all('a', class_ = 'search_result_row'):
         time.sleep(random.randint(2,4))
-        on_sale = discounts.find('div', class_='search_discount').span.text
         name = discounts.find('span', class_='title').text
+        on_sale = discounts.find('div', class_='search_discount').span.text
         price = discounts.find('div', class_= 'search_price').text
         price = price.replace('\n\t\t\t\t\t\t\t\t', '')
         price = price.replace('€', '€\t\t')
-        rounded_price = price[0:2]
+        rounded_price = price[:3]
         if price >= number:
             continue
         
@@ -84,11 +94,11 @@ def show_filtered_by_price(soup, number):
 def show_filtered_by_sale(soup, number):
     for discounts in soup.find_all('a', class_ = 'search_result_row'):
         time.sleep(random.randint(2,4))
-        on_sale = discounts.find('div', class_='search_discount').span.text
         name = discounts.find('span', class_='title').text
+        on_sale = discounts.find('div', class_='search_discount').span.text
         price = discounts.find('div', class_= 'search_price').text
-        rounded_sale = on_sale[0:2]
-        if rounded_sale >= number:
+        rounded_sale = on_sale[:3]
+        if rounded_sale <= number:
             continue
         
         price = price.replace('\n\t\t\t\t\t\t\t\t', '')
@@ -100,9 +110,23 @@ def show_filtered_by_sale(soup, number):
         print('\n')
 
 
+def export_to_csv(soup):
+    for discounts in soup.find_all('a', class_ = 'search_result_row'):
+        time.sleep(random.randint(2,4))
+        name = discounts.find('span', class_='title').text
+        on_sale = discounts.find('div', class_='search_discount').span.text
+        price = discounts.find('div', class_= 'search_price').text
+        price = price.replace('\n\t\t\t\t\t\t\t\t', '')
+        price = price.replace('€', '€\t\t')
+        csv_file = open('steam_scrape.csv', 'w')
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(['Name', 'Discount', 'Prices'])
+        csv_writer.writerow([name, on_sale, price])
+
+
 def main():
     while True:
-
+        print_header()
         soup = making_soup()
         menu(soup)
 
@@ -112,18 +136,3 @@ if __name__ == "__main__":
 
 
 
-
-# csv_file = open('steam_scrape.csv', 'w')
-
-
-
-
-# print(soup.prettify())
-
-# discounts = soup.find('search_discount')
-
-# response = requests.get(url)
-
-# text = response.text
-
-# print(text)
